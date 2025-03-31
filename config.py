97863c49f369eea0effef3af849f2a32876b1c37
olekjanowiak <olekjanowiak916@gmail.com>
2025-03-31 11:46:50 +0200

    dodano wiele optimów, zmieniono funkcję fitness, wizualizacja do poprawy

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ import numpy as np
 # -------------------
 # PARAMETRY POPULACJI
 # -------------------
-N = 50           # liczba osobników w populacji
+N = 150           # liczba osobników w populacji
 n = 2            # wymiar przestrzeni fenotypowej
 
 # --------------------
 # PARAMETRY MUTACJI
 # --------------------
-mu = 0.1         # prawdopodobieństwo mutacji dla osobnika
+mu = 0.5         # prawdopodobieństwo mutacji dla osobnika
 mu_c = 0.5       # prawdopodobieństwo mutacji konkretnej cechy, jeśli osobnik mutuje
-xi = 0.1         # odchylenie standardowe w rozkładzie normalnym mutacji
+xi = 0.5         # odchylenie standardowe w rozkładzie normalnym mutacji
 
 # --------------------
 # PARAMETRY SELEKCJI
@@ -25,12 +25,13 @@ threshold = 0.1  # przykładowy próg do selekcji progowej (do ewentualnego uży
 # PARAMETRY ŚRODOWISKA
 # --------------------
 # Początkowe alpha(t)
-alpha0 = np.array([0.0, 0.0])  
+alpha0 = [np.array([0.0, 0.0])]
 # Wektor kierunkowej zmiany c
 c = np.array([0.01, 0.01])     
-delta = 0.01    # odchylenie standardowe dla fluktuacji
-max_generations = 100  # liczba pokoleń do zasymulowania
+delta = 0.1    # odchylenie standardowe dla fluktuacji
+max_generations = 300  # liczba pokoleń do zasymulowania
 circle_radius = 0.5
+max_num_optims = 3 # finalna liczba optimów fenotypowych
 
 # ----------------------
 # PARAMETRY REPRODUKCJI
--- visualization.py
+++ visualization.py
@@ -8,28 +8,34 @@ def plot_population(population, alpha, generation, circle_radius, save_path=None
     Można zarówno wyświetlać (show_plot=True), jak i zapisywać obraz (save_path != None).
     circle_radius - skaluje promień okręgów.
     """
-    x = [ind.get_phenotype()[0] for ind in population.get_individuals()]
-    y = [ind.get_phenotype()[1] for ind in population.get_individuals()]
+    population_xs = [ind.get_phenotype()[0] for ind in population.get_individuals()]
+    population_ys = [ind.get_phenotype()[1] for ind in population.get_individuals()]
+
+    optima_xs = [optim[0] for optim in alpha]
+    optima_ys = [optim[1] for optim in alpha]
+
     plt.figure(figsize=(6, 6))
 
     # Rysowanie okręgów wokół optimum
     distances = [5, 3, 1]
     labels = ['5 szans na potomka', '3 szanse na potomka', '1 szansa na potomka'][::-1]
-    gradient = [0.22, 0.3, 0.5]
+    #gradient = [0.22, 0.3, 0.5]
+    colors = ["#c7e3c7", "#8ac58a", "#44a244"]
+    
+    for r, c, l in zip(distances, colors, labels):
+        for optim in alpha:
+            circle = plt.Circle(optim, r * circle_radius, color=c, fill=True, zorder=1, label=l)
+            plt.gca().add_patch(circle)
 
-    for r, g, l in zip(distances, gradient, labels):
-        circle = plt.Circle(alpha, r * circle_radius, color='green', alpha=g, fill=True, zorder=1, label=l)
-        plt.gca().add_patch(circle)
-
-    plt.scatter(x, y, label="Osobnik", alpha=0.7)
-    plt.scatter([alpha[0]], [alpha[1]], color='black', label="Optimum", marker='X')
+    plt.scatter(population_xs, population_ys, label="Osobnik", alpha=0.7)
+    plt.scatter(optima_xs, optima_ys, color='black', label="Optimum", marker='X')
 
 
     plt.title(f"Pokolenie: {generation}")
     # plt.text(0,5.5, f"Liczba osobników: {len(population.get_individuals())}",
     #          fontsize=10, ha='center', color='black', zorder=6)
-    plt.xlim(alpha[0] - 6, alpha[0] + 6)
-    plt.ylim(alpha[1] - 6, alpha[1] + 6)
+    plt.xlim(alpha[0][0] - 6, alpha[0][0] + 6)
+    plt.ylim(alpha[0][1] - 6, alpha[0][1] + 6)
     plt.legend(loc='upper right')
     plt.grid(True, linestyle='--', alpha=0.5)
     plt.tight_layout()
