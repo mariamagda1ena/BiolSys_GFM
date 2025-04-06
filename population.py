bc0460948bd03d83291736c419cf9af1f4ff920b
olekjanowiak <olekjanowiak916@gmail.com>
2025-04-06 14:41:53 +0200

    populacja zaczyna koło początkowego optimum, nie wokół [0.,0.]

--- population.py
+++ population.py
@@ -8,16 +8,17 @@ class Population:
     Klasa przechowuje listę osobników (Individual)
     oraz pomaga w obsłudze różnych operacji na populacji.
     """
-    def __init__(self, size, n_dim):
+    def __init__(self, alpha_init, size, n_dim):
         """
         Inicjalizuje populację losowymi fenotypami w n-wymiarach.
+        :param alpha: fenotyp początkowy, wokół którego inizjalizujemy populację
         :param size: liczba osobników (N)
         :param n_dim: wymiar fenotypu (n)
         """
         self.individuals = []
         for _ in range(size):
             # przykładowo inicjalizujemy fenotypy w okolicach [0, 0, ..., 0]
-            phenotype = np.random.normal(loc=0.0, scale=1.0, size=n_dim)
+            phenotype = np.random.normal(loc=alpha_init, scale=1.0, size=n_dim)
             self.individuals.append(Individual(phenotype))
 
     def get_individuals(self):
